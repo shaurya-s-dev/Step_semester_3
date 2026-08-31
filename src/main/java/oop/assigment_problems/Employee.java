c672694102b041faa1698293cddab8042d4ef6f0
Shaurya Singh <shauryasingh8d@gmail.com>
2026-08-31 21:42:44 +0530

    F1-F5: OOP assignment solutions

---
+++ src/main/java/oop/assigment_problems/Employee.java
@@ -0,0 +1,21 @@
+package oop.assigment_problems;
+
+public class Employee {
+    private int empId;
+    private String empName;
+    private double salary;
+
+    public Employee(int empId, String empName, double salary) {
+        this.empId = empId;
+        this.empName = empName;
+        this.salary = salary;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+}

f7ff1eb1a53b62a66b9bb858631d9d4b9ab29936
Shaurya Singh <shauryasingh8d@gmail.com>
2026-09-03 19:09:08 +0530

    Clean main: keep only README.md (code moved to feature branches)

--- src/main/java/week4/assignment_problems/PremiumAccount.java
+++
@@ -1,9 +0,0 @@
-package week4.assignment_problems;
-
-public class PremiumAccount extends DeliveryAccount {
-    public PremiumAccount(String studentId, double orderValue) {
-        super(studentId, orderValue);
-    }
-
-    // Override if needed; not required.
-}
--- src/main/java/week4/class_problems/SleeperCoachAccount.java
+++
@@ -1,9 +0,0 @@
-package week4.class_problems;
-
-public class SleeperCoachAccount extends BusTicketAccount {
-    public SleeperCoachAccount(String bookingId, double ticketFare) {
-        super(bookingId, ticketFare);
-    }
-
-    // Override penalty if needed? Not required, but we keep.
-}
